a8e9fed11b1ed36de8d47dabe6befa88d490f9c4
Cristian Araujo <araujocristian@live.com>
2021-03-21 16:15:00 -0300

    Finalizando componete de cadastro

--- forms/src/app/demos/reactive-forms/cadastro/cadastro.component.ts
+++ forms/src/app/demos/reactive-forms/cadastro/cadastro.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-cadastro',
-  templateUrl: './cadastro.component.html',
-  styles: []
+  selector: "app-cadastro",
+  templateUrl: "./cadastro.component.html",
+  styles: [],
 })
 export class CadastroComponent implements OnInit {
+  cadastroForm: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.cadastroForm = new FormGroup({
+      nome: new FormControl(""),
+      cpf: new FormControl(""),
+      email: new FormControl(""),
+      senha: new FormControl(""),
+      senhaConfirmacao: new FormControl(""),
+    });
   }
 
+  adicionarUsuario() {
+    let formValue = this.cadastroForm.value;
+
+    console.log("Dados do formulário:", formValue);
+  }
 }
