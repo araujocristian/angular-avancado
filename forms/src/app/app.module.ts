bf7d1d46c7ad8e2e78a533d39dede8e4b54ea9d8
Cristian Araujo <araujocristian@live.com>
2021-03-21 12:40:25 -0300

    Configurando rota de cadastro

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { APP_BASE_HREF } from '@angular/common';
-import { FormsModule }   from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { APP_BASE_HREF } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { MenuComponent } from './navegacao/menu/menu.component';
-import { HomeComponent } from './navegacao/home/home.component';
-import { FooterComponent } from './navegacao/footer/footer.component';
-import { SobreComponent } from './institucional/sobre/sobre.component';
-import { rootRouterConfig } from './app.routes';
-import { CadastroComponent } from './demos/reactive-forms/cadastro/cadastro.component';
+import { AppComponent } from "./app.component";
+import { MenuComponent } from "./navegacao/menu/menu.component";
+import { HomeComponent } from "./navegacao/home/home.component";
+import { FooterComponent } from "./navegacao/footer/footer.component";
+import { SobreComponent } from "./institucional/sobre/sobre.component";
+import { rootRouterConfig } from "./app.routes";
+import { CadastroComponent } from "./demos/reactive-forms/cadastro/cadastro.component";
 
 @NgModule({
   declarations: [
@@ -19,16 +19,15 @@ import { CadastroComponent } from './demos/reactive-forms/cadastro/cadastro.comp
     HomeComponent,
     FooterComponent,
     SobreComponent,
-    CadastroComponent
+    CadastroComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    [RouterModule.forRoot(rootRouterConfig, { useHash: false})]
+    ReactiveFormsModule,
+    RouterModule.forRoot(rootRouterConfig, { useHash: false }),
   ],
-  providers: [
-    {provide: APP_BASE_HREF, useValue: '/'}
-  ],
-  bootstrap: [AppComponent]
+  providers: [{ provide: APP_BASE_HREF, useValue: "/" }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
